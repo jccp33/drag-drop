0d7b10994531da65d1773348bee0f1d00a474902
CARLOS CARDENAS <jccp33@hotmail.com>
2023-03-08 12:31:14 -0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const ALL_FIELDS = document.getElementById('div-all-fields');
+
+function dragEnter(e) {
+    e.preventDefault();
+    //e.target.classList.add('drag-over');
+}
+
+function dragOver(e) {
+    e.preventDefault();
+    //e.target.classList.add('drag-over');
+}
+
+function dragLeave(e) {
+    //e.target.classList.remove('drag-over');
+}
+
+function drop(e) {
+    const id = e.dataTransfer.getData('text/plain');
+    const draggable = document.getElementById(id);
+    const clone = draggable.cloneNode(true);
+    clone.innerText += " X";
+    clone.addEventListener('click', (event)=>{
+        e.target.removeChild(clone);
+    });
+    // add it to the drop target
+    e.target.appendChild(clone);
+}
+
+window.addEventListener('load', ()=>{
+    ALL_FIELDS.innerHTML = "";
+    for(let i=0; i<46; i++){
+        ALL_FIELDS.innerHTML += `<div class="div-field" draggable="true" id="${'field-'+(i+1)}">${'field-'+(i+1)}</div>`;
+    }
+
+    let div_fields = document.getElementsByClassName('div-field');
+    for(let i=0; i<div_fields.length; i++){
+        div_fields[i].addEventListener('dragstart', (e)=>{
+            e.dataTransfer.setData('text/plain', e.target.id);
+            //console.log(e);
+        });
+    }
+
+    let boxes = document.getElementsByClassName('box');
+    for(let i=0; i<boxes.length; i++){
+        boxes[i].addEventListener('dragenter', dragEnter)
+        boxes[i].addEventListener('dragover', dragOver);
+        boxes[i].addEventListener('dragleave', dragLeave);
+        boxes[i].addEventListener('drop', drop);
+    }
+});
