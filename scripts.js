5622b5d58c1e07e2110a4852a18df5fbd84c8a4f
CARLOS CARDENAS <jccp33@hotmail.com>
2023-03-10 13:07:38 -0700

    second commit

--- scripts.js
+++ scripts.js
@@ -18,20 +18,55 @@ function dragLeave(e) {
 
 function drop(e) {
     const id = e.dataTransfer.getData('text/plain');
-    const draggable = document.getElementById(id);
-    const clone = draggable.cloneNode(true);
-    clone.innerText += " X";
-    clone.addEventListener('click', (event)=>{
-        e.target.removeChild(clone);
+    const clone = document.getElementById(id).cloneNode(true);
+
+    clone.title = "";
+    clone.childNodes.forEach(node => {
+        if(node.tagName === 'I'){
+            node.title = 'DELETE';
+            node.addEventListener('click', ()=>{
+                e.target.removeChild(clone);
+            });
+        }
     });
-    // add it to the drop target
+    
     e.target.appendChild(clone);
+}
+
+function SaveSettings(){
+    let div_process1 = document.getElementById('div-process-1');
+    let fields = "";
+    div_process1.childNodes.forEach(node => {
+        node.childNodes.forEach(n => {
+            if(n.tagName=="LABEL" || n.tagName=="INPUT"){
+                if(n.tagName == "LABEL"){
+                    fields += n.innerText.trim();
+                }
+                if(n.tagName == "INPUT"){
+                    if(n.checked){
+                        fields += ":mandatory|";
+                    }else{
+                        fields += ":no-mandatory|";
+                    }
+                }
+            }
+        });
+    });
+    console.log(fields);
 }
 
 window.addEventListener('load', ()=>{
     ALL_FIELDS.innerHTML = "";
     for(let i=0; i<46; i++){
-        ALL_FIELDS.innerHTML += `<div class="div-field" draggable="true" id="${'field-'+(i+1)}">${'field-'+(i+1)}</div>`;
+        ALL_FIELDS.innerHTML += `
+        <div class="div-field" draggable="true" id="${'field-'+(i+1)}" title="Drag & Drop">
+            <i class="fa fa-close"></i>
+            <label for="${'check-field-'+(i+1)}">
+                ${'field-'+(i+1)}
+            </label>
+            <input type="checkbox" id="${'check-field-'+(i+1)}" name="${'check-field-'+(i+1)}">
+        </div>
+        `;
     }
 
     let div_fields = document.getElementsByClassName('div-field');
